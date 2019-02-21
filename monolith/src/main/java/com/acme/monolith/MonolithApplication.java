a9a052f081d24baa1b4cc0b88fd6a2aa02f975d1
Ashok Koyi <ashok.koyi@gmail.com>
2019-02-21 07:15:51 +0530

    Init

---
+++ monolith/src/main/java/com/acme/monolith/MonolithApplication.java
@@ -0,0 +1,11 @@
+package com.acme.monolith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonolithApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(MonolithApplication.class, args);
+  }
+}
